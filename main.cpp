6347665adfce16686aa31fbeebbba2fe4698511a
Fanfara1329 <t985978@gmail.com>
2023-05-28 23:44:47 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void time() {
+    cout << "GOOOOOOOOOOOOOOO\n";
+    string s;
+    time_t start, end;
+    double ans = 0;
+    time(&start);
+    getline(cin, s);
+    ans += s.size();
+    while (s != "") {
+        getline(cin, s);
+        ans += s.size();
+    }
+    time(&end);
+    cout << "Yors typing speed  -  one char / " << ans / double(end - start) << " seconds\n";
+    return;
+}
+
+int main() {
+    cout
+            << "Aloha! Now you can find out how fast you type. Just press enter to continue, the timer get start instantly, so get ready\n"
+            << "P.S. Press enter twice when you`re done.\n";
+    string h;
+    getline(cin, h);
+    while (h == " ") {
+        time();
+        getline(cin, h);
+        cout << h << "   h\n";
+    }
+    return 0;
+}
